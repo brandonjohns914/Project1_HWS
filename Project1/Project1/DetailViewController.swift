38c662f21ba3eae862f02335163b11e6d052a616
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2023-05-08 14:26:24 -0700

    Add files via upload

--- Project1/Project1/DetailViewController.swift
+++ Project1/Project1/DetailViewController.swift
@@ -9,22 +9,32 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet var imageView: UIImageView!
+    var selectedPictureNumber = 0
+    var totalPictures = 0
     
-    var selected_Image: String?
+    @IBOutlet var imageView: UIImageView!
+                                                                                //@IBOutlet == connection between code and interface builder
+                                                                                // imageView: name of the UIImageView from IB
+                                                                                // UIImageView! = delcaring property of UIIMageView
+                                                                                //                force unwrapped 
+    
+    var selected_Image: String?                                                 // selected image might exist or might not 
     
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        title = selected_Image
-        navigationItem.largeTitleDisplayMode = .never
         
-        if let image_to_load = selected_Image
-        {
+        
+        title = "this image is \(selectedPictureNumber) of \(totalPictures)"                                // title to be shown
+        navigationItem.largeTitleDisplayMode = .never                            // title is smaller than on the main page
+        
+        if let image_to_load = selected_Image                                   // checks and unwraps selected_Image
+        {                                                                       // if nil imageView.Image will never run
+            
             imageView.image = UIImage(named: image_to_load)
+                                                                                // imageView.image exists then place it into image_to_load then pass it to the UIImage
         }
         
     }
--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -7,22 +7,32 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UITableViewController
+{
 
-    var pictures: [String] = []
+    var pictures: [String] = []                                                             // array is created when the ViewController screen is created
+                                                                                            // array exists as long as the screen exists
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    override func viewDidLoad() {                                                           // override = chagne apples default behavior
+        super.viewDidLoad()                                                                 // super means run Apples code before mine
+
         title = "Ellie Viewer"
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        let file_manger = FileManager.default
-        let path_image = Bundle.main.resourcePath!
-        let images = try! file_manger.contentsOfDirectory(atPath: path_image)
+        let file_manger = FileManager.default                                               // file_manger = the value returned by Filemanger.default
+        
+        let path_image = Bundle.main.resourcePath!                                          //path_image = the resource path of the apps bundle
+                                                                                            // bundle =  directory containing all the compiled progam and the assets
+                                                                                            // where  can i find all the images added to my app
+                                                                                            // force unwrapping is ok because if it fails the app isnt reading from its own files
+                                                                        
+        
+        let images = try! file_manger.contentsOfDirectory(atPath: path_image)               // images = the contents of the directory from path_image
+                                                                                            // images = array of strings that the names of the files
         
         for image in images.sorted()
         {
+            
             if image.hasPrefix("Ellie")
             {
                 
@@ -31,26 +41,59 @@ class ViewController: UITableViewController {
         }
         
     }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+                                                                                           
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {                                                                                        // returns Int of how many rows in the table
+                                                                                             // _ tableView: UITableView
+                                                                                                // _  = first paramter not being passed uintg a name
+                                                                                                    // can be the name of the table being worked with if multiple tables
+                                                                                               //tableView = name of the table view in the method
+                                                                                                //UITableView = the data type
+        
+                                                                                            // numberOfRowsInSection section: Int
+                                                                                                //numberOfRowsInSection = Action (how many rows)
+                                                                                                // section: = how many ways to split up (how Contacts split up by names)
         return pictures.count
-    }
+    }// how many rows
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //"Picture" is the Cell name in Interface Builder
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {                                                                                       // returns UITableViewCell
+                                                                                                // the return type is Picture created in Interface Builder
+                                                                                            //cellForRowAt indexPath: IndexPath
+                                                                                                //cellForRowAt provide a specific row
+                                                                                                    // the row is indexPath
+                                                                                                //IndexPath == Type which is section and row number
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+                                                                                            // rows move on and off the screen using dequeueResuableCell to save data
+                                                                                            // cell == whats being dequeued
+                                                                                            //      == whats being displayed
+                                                                                            //"Picture" == the Cell name in Interface Builder
         
         cell.textLabel?.text = pictures[indexPath.row]
+                                                                                            // cell is a textLabel Optional
+                                                                                            // only show the cell if it exists
+                                                                                            // pictures[indexPath.row]
+                                                                                                // textlabel to be of the picture in the pictures array
+                                                                                                // indexPath.row == row number being asked to load
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //"Detail" is the storyboard ID for the detail view controller
+                                                                                            //loading "Detail" view controller and typecasting to DetailViewController
         if let view_controller = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController
-        {
-            view_controller.selected_Image = pictures[indexPath.row]
-            navigationController?.pushViewController(view_controller, animated: true)
+        {                                                                                   // if let protects just incase of failure
+                                                                                            //      stoyboard could not exist
+                                                                                            //      "Detail" could be wrong name
+                                                                                            //      DetailViewController oculd be the wrong viewcontroller)
+                                                                                            
+                                                                                            //"Detail" is the storyboard ID for the DetailViewController
+            
+            view_controller.selected_Image = pictures[indexPath.row]                        // sets selected_Image and its row
+            view_controller.totalPictures = pictures.count                                  // passes picture count
+            view_controller.selectedPictureNumber = indexPath.row + 1                           // passes the picture in place
+                                                                                            
+            navigationController?.pushViewController(view_controller, animated: true)       // put it on the nativation cotnroller
         }
     }
 }
